246035b9779a50431aae6b45a1bebbf5d9dcbb29
Gneitaimei <120438581+Gneitaimei@users.noreply.github.com>
2022-12-18 16:23:27 +0800

    Add files via upload

---
+++ day7/day7/test.c
@@ -0,0 +1,187 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+//猜数字游戏
+int guess = 0;
+void menu()
+{
+	printf("############################\n");
+	printf("####  1.play   2.exit  #####\n");
+	printf("############################\n");
+
+}
+void game()
+{
+	int ret = 0;
+	ret= rand()%100+1;
+	while (1)
+	{
+		printf("请猜数字：>");
+		while (scanf("%d", &guess) == 0)
+		{
+			while (getchar() != '\n');
+			printf("您输入了非法字符，请重新输入：\n");
+		}
+		
+		if (guess > ret)
+		{
+			printf("大了\n");
+		}
+		else if (guess < ret)
+		{
+			printf("小了\n");
+		}
+		else if (guess == ret)
+		{
+			printf("猜对了！\n\n\n");
+			break;
+		}
+		
+	}
+		
+}
+
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择>:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 2:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择无效\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
+
+
+
+
+//9*9乘法表
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		int j = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-2d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int j = 0;
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j > sqrt(i))
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (year % 4 == 0 && year % 100 != 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//		else if (year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int m = 12;
+//	int n = 16;
+//	int r = 0;
+//	scanf("%d%d", &m, &n);
+//	while (r=m%n)
+//	{
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		int tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//	printf("%d %d %d", a, b, c);
+//	return 0;
+//}
