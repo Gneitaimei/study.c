82a7a9d75cc288a2603688bd16e1e010456bdac7
Gneitaimei <120438581+Gneitaimei@users.noreply.github.com>
2022-12-21 17:31:55 +0800

    Add files via upload

---
+++ day10/day10/test.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+void bubble_sort(int arr[],int sz)
+{
+	int flag = 1;
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz-1-i; j++)
+		{
+			if (arr[j]>arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] =arr[j + 1];
+				arr[j + 1] = tmp;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			break;
+		}
+	}
+}
+
+
+int main()
+{
+	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	bubble_sort(arr,sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
+
+//二维数组
+//int main()
+//{
+//	//行可以省略；列不能省略
+//	int arr[3][4] = { { 1, 2, 3 }, { 4, 5 } };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr[%d][%d]=%p\n", i, j, &arr[i][j]);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("&arr[%d]=%p\n",i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char arr[] = { "acv\0er" };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
